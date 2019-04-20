37233a282797010ea074c5b8b180dcd12f543e6a
mmoccaro <mmoccaro@localhost.localdomain>
2019-04-20 18:29:51 -0400

    Adding inital GTK+ 3.0 setup.

--- source/Syslog_S1.cpp
+++ source/Syslog_S1.cpp
@@ -13,10 +13,28 @@
 
 //Directives
 #include<iostream>
+#include<gtk/gtk.h>
+
+void destroy( GtkWidget *widget,gpointer   data )
+{
+   gtk_main_quit ();
+}
 
 //Main
-int main()
+int main(int argc, char **argv)
 {
+   GtkWidget *window;
+   gtk_init (&argc, &argv);
+ 
+   window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+ 
+   g_signal_connect (window, "destroy",
+   G_CALLBACK (destroy), NULL);
+ 
+   gtk_widget_show_all(window); 
+ 
+   gtk_main ();
+
 	//Exit Status
 	return 0;
 }
