1b1fc1440f455c7765f0c3b38789cd11e3dc3ce7
mmoccaro <mmoccaro@localhost.localdomain>
2019-04-20 18:21:29 -0400

    Added header to main source file.

--- source/Syslog_S1.cpp
+++ source/Syslog_S1.cpp
@@ -1,6 +1,22 @@
+/* =======================
+ *
+ * Author: Matthew Moccaro
+ * File:   Syslog_S1.cpp
+ * Type:   C++ Soure File
+ * 
+ * Creation Date: 4/20/19
+ *
+ * Description: Main source 
+ * file for project.
+ * 
+ * ====================== */
+
+//Directives
 #include<iostream>
 
+//Main
 int main()
 {
+	//Exit Status
 	return 0;
 }
